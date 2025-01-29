894646901aeca22bc9b8231161feeecd536f6412
Fanny SAEZ <saez.fanny.63@gmail.com>
2025-01-29 19:42:47 +0100

    Update script2.js

--- assets/exercices-2/script2.js
+++ assets/exercices-2/script2.js
@@ -2,10 +2,17 @@
 // Faire une fonction qui prend en paramètre un prix HT unitaire d’un produit, et son nombre de produit.
 // On veut connaitre le total TTC.
 console.log("")
-console.log("")
-console.log("")
-console.log("")
+function calculerTotalTTC(prixHT, quantite, tauxTVA = 20) {
+    if (prixHT < 0 || quantite < 0) {
+        return "Les valeurs doivent être positives.";
+    }
+    
+    let totalHT = prixHT * quantite;
+    let totalTTC = totalHT * (1 + tauxTVA / 100);
+    
+    return totalTTC.toFixed(2); // Retourne le total avec 2 décimales
+}
 
-console.log("__________________________________________________________________________________________")
-console.log("Exercice-3");
-console.log("__________________________________________________________________________________________")
+// Exemple d'utilisation
+console.log(calculerTotalTTC(100, 2)); // 240.00 si TVA = 20%
+console.log(calculerTotalTTC(50, 5, 10)); // 275.00 si TVA = 10%
